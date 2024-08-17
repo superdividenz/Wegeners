1e85c200756a4a71659c6646b57d78752eda7132
P Driscoll <75807121+superdividenz@users.noreply.github.com>
2024-08-16 22:27:20 -0500

    Delete frontend directory

--- frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = () => (
-  <footer className="bg-gray-200 p-4 text-center">
-    <p>&copy; 2024 My Band App</p>
-  </footer>
-);
-
-export default Footer;
